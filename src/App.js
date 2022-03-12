54661bf891f06ca76c1393f1b1dc9c15baef69c5
Diogo Gil <diogo.afg13@gmail.com>
2022-03-12 17:22:22 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed by DGTech :)
+          I was changed by DGTech :) :)
         </a>
       </header>
     </div>
