cdc616b55db2b210fb55f5441c1da15182b325b9
Diogo Gil <diogo.afg13@gmail.com>
2022-03-12 16:36:46 +0000

    changed test cases to allow travis ci to not fail

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("I was changed one the Feature Branch");
   expect(linkElement).toBeInTheDocument();
 });
 
 
 test('renders learn react link 2', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("I was changed one the Feature Branch");
   expect(linkElement).toBeInTheDocument();
 });
