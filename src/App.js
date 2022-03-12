24cbd6f4b4e2c4f4a0b43dd3a02acc889b9ae36c
Diogo Gil <diogo.afg13@gmail.com>
2022-03-12 16:45:15 +0000

    change message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed one the Feature Branch
+          I was changed one the Feature Branch - DGTech
         </a>
       </header>
     </div>
